b1dab4b21865a97003be863311cfd9fe68952043
Brandt Redd <brandt@redd.org>
2017-05-25 23:46:56 -0600

    Full function with unit test.

---
+++ ConsoleHelper.cs
@@ -0,0 +1,86 @@
+﻿/*
+---
+# Metadata in MicroYaml format. See http://filemeta.org and http://schema.org
+name: ConsoleHelper.cs
+description: Helper class for console applications in C#
+url: https://github.com/FileMeta/ConsoleHelper/raw/master/ConsoleHelper.cs
+version: 1.0
+keywords: codebit
+dateModified: 2017-05-25
+license: http://creativecommons.org/publicdomain/zero/1.0/
+...
+*/
+/*
+Creative Commons CC0
+To the extent possible under law, Brandt Redd has waived all copyright and
+related or neighboring rights to ConsoleHelper.
+This work is published from: United States
+*/
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Win32Interop
+{
+
+    /// <summary>
+    /// Set of static properties and methods that make console applicaitons operate more smoothly.
+    /// </summary>
+    static class ConsoleHelper
+    {
+
+        #region Private Stuff
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint GetConsoleProcessList(uint[] ProcessList, uint ProcessCount);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        #endregion Private Stuff
+
+        #region Public Stuff
+
+        /// <summary>
+        /// Returns true if application is the sole owner of the current console.
+        /// </summary>
+        public static bool IsSoleConsoleOwner
+        {
+            get
+            {
+                uint[] procIds = new uint[4];
+                uint count = GetConsoleProcessList(procIds, (uint)procIds.Length);
+                return count <= 1;
+            }
+        }
+
+        /// <summary>
+        /// If applicaiton is the sole console owner, prompts the user to press
+        /// any key before returning - presumably for the application to exit.
+        /// </summary>
+        public static void PromptAndWaitIfSoleConsole()
+        {
+            if (IsSoleConsoleOwner)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey(true);
+            }
+        }
+
+        /// <summary>
+        /// Brings the console to the front
+        /// </summary>
+        public static void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+
+        #endregion Public Stuff
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Unit test for ConsoleHelper class.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Win32Interop;
+
+namespace UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // First set console to unicode.
+            Console.WriteLine("Setting console to unicode mode...");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Write some unicode text
+            Console.WriteLine("The following includes unicode characters.");
+            Console.WriteLine("Times: \u00D7");
+            Console.WriteLine("Omega: \u03A9");
+            Console.WriteLine("Pi: \u03C0");
+            Console.WriteLine("Pound, Euro, Yen: \u00A3 \u20AC \u00A5");
+            Console.WriteLine();
+
+            // Test bring console to front
+            Console.WriteLine("Sleeping for 5 seconds. Bring another window in front to test\r\nwhether application can bring itself to the front.");
+            System.Threading.Thread.Sleep(5000);
+            ConsoleHelper.BringConsoleToFront();
+            Console.WriteLine("Console in front.");
+            Console.WriteLine();
+
+            // See if sole console owner
+            if (ConsoleHelper.IsSoleConsoleOwner)
+            {
+                Console.WriteLine("Application is the sole console owner.\r\nIt should prompt before closing.");
+            }
+            else
+            {
+                Console.WriteLine("Application is not the sole console owner.\r\nIt will exit immediately.");
+            }
+
+            ConsoleHelper.PromptAndWaitIfSoleConsole();
+        }
+    }
+}
